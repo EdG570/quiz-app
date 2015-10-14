dbb57ff2dd74335f472481c36a28a36c27629660
Ed Glazenski <edglazenski@gmail.com>
2015-10-14 15:15:42 -0400

    Modified answer list elements but having issues losing styling on append

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 
 var userAnswer;
 var correctAnswer = questionOne.correctAnswer;
-var clicks = 1;
+var counter = 1;
+
+
 
 /*--Hides and shows elements for transition from landing content to quiz start content after clicking get started button--*/
 $('#start-btn').on('click', function(){
@@ -11,9 +13,10 @@ $('#start-btn').on('click', function(){
   $(this).siblings('h2').hide();
   $(this).hide();
 
+
   /*------Show Elements-----*/
   $(this).siblings('h3').show();
-  $(this).siblings('ul').show();
+  $(this).siblings('form').show();
   $(this).prev().show();
   $(this).parent().prev().children('h2').show();
   $(this).parent().prev().children('.progress-icons').show();
@@ -22,35 +25,57 @@ $('#start-btn').on('click', function(){
 
   $(this).parent().children('h3').append('<span id="remove"> ' + questions[0].question + '</span');
 
-  /*------Adds Answers to quiz-------*/
+  /*------Adds first question answers to quiz-------*/
   for(i = 0; i <= 3; i++){
-    $(this).prev().prev().append('<li><button class="answer-btn button">' + questions[0].answers[i] + '</button></li>');
+    $(this).prev().prev().append('<li><input type="checkbox" class="answer-btn button" id="answer-1" value="' + questions[0].answers[i] + '" hidden><label for="answer-1">' + questions[0].answers[i] + '</label></input></li>');
   }
+
 });
 
+ var hot = "toot";
 
-/*---Changes answer background on click ---*/
-$('.answer-btn').on('click', function(){
-  $(this).css('background-color', '#F5A623');
-  $(this).parent().siblings().children().css('background-color', '#777777');
-});
+$('form').submit(function(e){
+  e.preventDefault();
+  var answer = [];
 
-$('#submit-btn').on('click', function(){
-  $(this).parent().children('h3').children().remove();
+  console.log(hot);
+
+  $('input[type="checkbox"]:checked').each(function() {
+    answer.push($(this).val());
+  });
+  console.log(answer);
+  $('.output').text(answer);
+
   $(this).prev().children().remove();
-  $(this).parent().find('h3').append('<span id="question-color">Question ' + (clicks + 1) + ':</span><span> ' + questions[clicks].question + '</span');
+  $(this).find('ul').children().remove();
+  $(this).prev().append('<span id="question-color">Question ' + (counter + 1) + ':</span><span> ' + questions[counter].question + '</span');
 
   for(i = 0; i <= 3; i++){
-    $(this).prev().append('<li><button class="answer-btn button">' + questions[clicks].answers[i] + '</button></li>');
+    $(this).prev().append('<li><input type="checkbox" class="answer-btn button" id="answer-1" value="' + questions[counter].answers[i] + '" hidden><label for="answer-1">' + questions[counter].answers[i] + '</label></input></li>');
   }
-  return clicks++;
 
+  return counter++;
 });
 
-$('.answer-btn').click(function(){
 
-});
 
+
+/*--------------Audio files------------*/
+
+var audio = {
+   greeting:function() {
+          $('#greeting')[0].volume=0.5;
+          $('#greeting')[0].load();
+          $('#greeting')[0].play();
+    },
+    door:function() {
+          $('#door')[0].volume=0.5;
+          $('#door')[0].load();
+          $('#door')[0].play();
+    }
+};
+
+audio.greeting();
 
 
 });
