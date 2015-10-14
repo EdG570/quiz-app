8e3d5fa30aabcaffb63abfe8f0dc1ec6cc94f6e0
Ed Glazenski <edglazenski@gmail.com>
2015-10-14 19:36:04 -0400

    Changed answer elements to buttons

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ $('#start-btn').on('click', function(){
 
   /*------Show Elements-----*/
   $(this).siblings('h3').show();
-  $(this).siblings('form').show();
+  $(this).siblings('ul').show();
   $(this).prev().show();
   $(this).parent().prev().children('h2').show();
   $(this).parent().prev().children('.progress-icons').show();
@@ -27,8 +27,7 @@ $('#start-btn').on('click', function(){
 
   /*------Adds first question answers to quiz-------*/
   for(i = 0; i <= 3; i++){
-    $(this).prev().find('ul').append('<li><input type="checkbox" class="answer-btn button" id="answer-1" value="' + questions[0].answers[i] + '" hidden><label for="answer-1">' + questions[0].answers[i] + '</label></input></li>');
-    $('ul').trigger('create');
+    $(this).prev().prev().append('<li><button class="answer-btn">' + questions[0].answers[i] + '</button></li>');
   }
     
 });
@@ -36,23 +35,15 @@ $('#start-btn').on('click', function(){
  
 /*-------On submit answer is stored in array and current answers/question
           are removed and next question/answers are added----------*/
-$('form').submit(function(e){
-  e.preventDefault();
-  var answer = [];
-
-  $('input[type="checkbox"]:checked').each(function() {
-    answer.push($(this).val());
-  });
-  console.log(answer);
-  $('.output').text(answer);
+$('#submit-btn').on('click', function(){
+  
 
   $(this).prev().children().remove();
-  $(this).find('ul').find('li').remove();
-  $(this).prev().append('<span id="question-color">Question ' + (counter + 1) + ':</span><span> ' + questions[counter].question + '</span');
+  $(this).prev().prev().children().remove();
+  $(this).prev().prev().append('<span id="question-color">Question ' + (counter + 1) + ':</span><span> ' + questions[counter].question + '</span');
 
   for(i = 0; i <= 3; i++){
-    $(this).find('ul').append('<li><input type="checkbox" class="answer-btn button" id="answer-1" value="' + questions[counter].answers[i] + '" hidden><label for="answer-1">' + questions[counter].answers[i] + '</label></input></li>');
-    $('ul').trigger('create');
+    $(this).prev().append('<li><button class="answer-btn">' + questions[0].answers[i] + '</button></li>');
   }
 
   audio.door();
