b271b5b1f5f4c4a5105bc74c67640cb13b57b9e0
Ed Glazenski <edglazenski@gmail.com>
2015-10-15 23:29:34 -0400

    Added monster mash

--- js/app.js
+++ js/app.js
@@ -60,15 +60,19 @@ $('#feedback').on('click', '#next-btn', function(){
     var score = (numCorrect / numQuestions) * 100;
     if(numCorrect === numQuestions ) {
         showResults();
+        audio.monster();
       }
     else if(counter === 5 && score === 0) {
         showResults();
+        audio.monster();
     }
     else if(counter === 5 && score >= 60){
         showResults();
+        audio.monster();
     }
     else if(counter === 5 && score > 0){
         showResults();
+        audio.monster();
     }
     else {
         $('#feedback').children().slice(1).remove();
